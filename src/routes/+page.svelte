d9317b234ccd550e65cb4c0d8cf394520279aa81
Nguyen Minh Khanh <khanhnm222@gmail.com>
2026-02-02 21:39:56 +0700

    feat: setup tailwindcss and prettierrc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-2xl">Welcome to SvelteKit</h1>
+<p class="text-fuchsia-700">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
